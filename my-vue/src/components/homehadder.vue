0c52f6286c3d7b8d252ca3fa3d28205e6b6b2a96
zhaoposhanhewanduo <837309137@qq.com>
2018-04-10 10:00:37 +0800

    aaaaaaaa

---
+++ my-vue/src/components/homehadder.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul class = 'com-footer'>
+      <li>书架</li>
+      <li>书城</li>
+      <li>我的</li>
+    </ul>
+</template>
+<script>
+export default {
+  name: 'homehadder',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ my-vue/src/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <header-top></header-top>
+  </div>
+</template>
+
+<script>
+import header from '../components/homehadder'
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App1'
+    }
+  },
+  components:{
+     'header-top':header,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">  
+</style>
